1ab9f860d89eece1fa0b79cca06ea65d2cff5983
aliffirjatullah87-spec <aliffirjatullah87@gmail.com>
2026-08-15 10:39:35 +0700

    Disable legacy PWA cache to fix Android controls

--- sw.js
+++ sw.js
@@ -1,5 +1,11 @@
-const CACHE='binjas-alif-v7';
-const A=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
+// BINJAS ALIF: disable legacy service worker so the app always loads the current GitHub Pages files.
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', event => {
+  event.waitUntil((async () => {
+    const keys = await caches.keys();
+    await Promise.all(keys.map(k => caches.delete(k)));
+    await self.registration.unregister();
+    const clients = await self.clients.matchAll({type:'window'});
+    clients.forEach(client => client.postMessage({type:'BINJAS_REFRESH'}));
+  })());
+});
