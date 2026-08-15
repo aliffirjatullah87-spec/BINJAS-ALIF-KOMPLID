2959ad9bbb0eac93c91fac5a7a76f27c7e965887
aliffirjatullah87-spec <aliffirjatullah87@gmail.com>
2026-08-15 09:17:50 +0700

    BINJAS ALIF v3.0 — refresh PWA cache

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='binjas-alif-v6';
+const CACHE='binjas-alif-v7';
 const A=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
