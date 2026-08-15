7ef702e5434966bc1de7986ab1de748a8a8755d7
aliffirjatullah87-spec <aliffirjatullah87@gmail.com>
2026-08-15 09:08:03 +0700

    Force mobile app cache refresh and update service worker

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-const CACHE='binjas-alif-v3';const A=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='binjas-alif-v6';
+const A=['./','./index.html','./manifest.json','./sw.js','./icon.svg'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();caches.open(CACHE).then(c=>c.put(e.request,copy)).catch(()=>{});return r}).catch(()=>caches.match(e.request))));
